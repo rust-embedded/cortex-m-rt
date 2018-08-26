39277c86f65a5c4513598b516846b2bcefea17ed
Emil Fresk <emil.fresk@gmail.com>
2018-08-26 20:50:01 +0200

    Error in if statement

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 main() {
 
-    if [ $TARGET != x86_64-unknown-linux-gnu ]; then
+    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
         mkdir mdcheck
         curl https://raw.githubusercontent.com/korken89/mdcheckr/master/mdcheckr -o mdcheck/mdcheckr
         chmod +x mdcheck/mdcheckr
