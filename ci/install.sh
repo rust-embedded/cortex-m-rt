fb47fd0f945448fcae38be59366f998e20e1af15
Emil Fresk <emil.fresk@gmail.com>
2018-08-26 20:47:23 +0200

    Remove use of sudo

--- ci/install.sh
+++ ci/install.sh
@@ -1,9 +1,12 @@
 set -euxo pipefail
 
 main() {
-    curl https://raw.githubusercontent.com/korken89/mdcheckr/master/mdcheckr -o mdcheckr
-    chmod +x mdcheckr
-    sudo cp mdcheckr /usr/local/bin
+
+    if [ $TARGET != x86_64-unknown-linux-gnu ]; then
+        mkdir mdcheck
+        curl https://raw.githubusercontent.com/korken89/mdcheckr/master/mdcheckr -o mdcheck/mdcheckr
+        chmod +x mdcheck/mdcheckr
+    fi
 
     if [ $TARGET != x86_64-unknown-linux-gnu ]; then
         rustup target add $TARGET
