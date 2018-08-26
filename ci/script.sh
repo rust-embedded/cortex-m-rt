92945c80f5e676443205c657be1091658512ee4b
Emil Fresk <emil.fresk@gmail.com>
2018-08-26 21:03:17 +0200

    Made a docs target

--- ci/script.sh
+++ ci/script.sh
@@ -1,88 +1,89 @@
 set -euxo pipefail
 
 main() {
-    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+    if [ $TARGET = docs ]; then
         find . -name '*.md' -print0 | xargs -0 mdcheckr
+    else
+        cargo check --target $TARGET
+
+        cargo check --target $TARGET --features device
+
+        local examples=(
+            alignment
+            minimal
+            main
+            override-exception
+            pre_init
+            state
+        )
+        local fail_examples=(
+            data_overflow
+        )
+        if [ $TRAVIS_RUST_VERSION = nightly ]; then
+            # linking with GNU LD
+            for ex in "${examples[@]}"; do
+                cargo rustc --target $TARGET --example $ex -- \
+                      -C link-arg=-nostartfiles \
+                      -C link-arg=-Wl,-Tlink.x
+
+                cargo rustc --target $TARGET --example $ex --release -- \
+                      -C link-arg=-nostartfiles \
+                      -C link-arg=-Wl,-Tlink.x
+            done
+            for ex in "${fail_examples[@]}"; do
+                ! cargo rustc --target $TARGET --example $ex -- \
+                      -C link-arg=-nostartfiles \
+                      -C link-arg=-Wl,-Tlink.x
+
+                ! cargo rustc --target $TARGET --example $ex --release -- \
+                      -C link-arg=-nostartfiles \
+                      -C link-arg=-Wl,-Tlink.x
+            done
+
+            cargo rustc --target $TARGET --example device --features device -- \
+                  -C link-arg=-nostartfiles \
+                  -C link-arg=-Wl,-Tlink.x
+
+            cargo rustc --target $TARGET --example device --features device --release -- \
+                  -C link-arg=-nostartfiles \
+                  -C link-arg=-Wl,-Tlink.x
+
+            # linking with rustc's LLD
+            for ex in "${examples[@]}"; do
+                cargo rustc --target $TARGET --example $ex -- \
+                      -C linker=rust-lld \
+                      -Z linker-flavor=ld.lld \
+                      -C link-arg=-Tlink.x
+
+                cargo rustc --target $TARGET --example $ex --release -- \
+                      -C linker=rust-lld \
+                      -Z linker-flavor=ld.lld \
+                      -C link-arg=-Tlink.x
+            done
+            for ex in "${fail_examples[@]}"; do
+                ! cargo rustc --target $TARGET --example $ex -- \
+                      -C linker=rust-lld \
+                      -Z linker-flavor=ld.lld \
+                      -C link-arg=-Tlink.x
+
+                ! cargo rustc --target $TARGET --example $ex --release -- \
+                      -C linker=rust-lld \
+                      -Z linker-flavor=ld.lld \
+                      -C link-arg=-Tlink.x
+            done
+
+            cargo rustc --target $TARGET --example device --features device -- \
+                  -C linker=rust-lld \
+                  -Z linker-flavor=ld.lld \
+                  -C link-arg=-Tlink.x
+
+            cargo rustc --target $TARGET --example device --features device --release -- \
+                  -C linker=rust-lld \
+                  -Z linker-flavor=ld.lld \
+                  -C link-arg=-Tlink.x
+        fi
     fi
 
-    cargo check --target $TARGET
-
-    cargo check --target $TARGET --features device
-
-    local examples=(
-        alignment
-        minimal
-        main
-        override-exception
-        pre_init
-        state
-    )
-    local fail_examples=(
-        data_overflow
-    )
-    if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        # linking with GNU LD
-        for ex in "${examples[@]}"; do
-            cargo rustc --target $TARGET --example $ex -- \
-                  -C link-arg=-nostartfiles \
-                  -C link-arg=-Wl,-Tlink.x
-
-            cargo rustc --target $TARGET --example $ex --release -- \
-                  -C link-arg=-nostartfiles \
-                  -C link-arg=-Wl,-Tlink.x
-        done
-        for ex in "${fail_examples[@]}"; do
-            ! cargo rustc --target $TARGET --example $ex -- \
-                  -C link-arg=-nostartfiles \
-                  -C link-arg=-Wl,-Tlink.x
-
-            ! cargo rustc --target $TARGET --example $ex --release -- \
-                  -C link-arg=-nostartfiles \
-                  -C link-arg=-Wl,-Tlink.x
-        done
-
-        cargo rustc --target $TARGET --example device --features device -- \
-              -C link-arg=-nostartfiles \
-              -C link-arg=-Wl,-Tlink.x
-
-        cargo rustc --target $TARGET --example device --features device --release -- \
-              -C link-arg=-nostartfiles \
-              -C link-arg=-Wl,-Tlink.x
-
-        # linking with rustc's LLD
-        for ex in "${examples[@]}"; do
-            cargo rustc --target $TARGET --example $ex -- \
-                  -C linker=rust-lld \
-                  -Z linker-flavor=ld.lld \
-                  -C link-arg=-Tlink.x
-
-            cargo rustc --target $TARGET --example $ex --release -- \
-                  -C linker=rust-lld \
-                  -Z linker-flavor=ld.lld \
-                  -C link-arg=-Tlink.x
-        done
-        for ex in "${fail_examples[@]}"; do
-            ! cargo rustc --target $TARGET --example $ex -- \
-                  -C linker=rust-lld \
-                  -Z linker-flavor=ld.lld \
-                  -C link-arg=-Tlink.x
-
-            ! cargo rustc --target $TARGET --example $ex --release -- \
-                  -C linker=rust-lld \
-                  -Z linker-flavor=ld.lld \
-                  -C link-arg=-Tlink.x
-        done
-
-        cargo rustc --target $TARGET --example device --features device -- \
-              -C linker=rust-lld \
-              -Z linker-flavor=ld.lld \
-              -C link-arg=-Tlink.x
-
-        cargo rustc --target $TARGET --example device --features device --release -- \
-              -C linker=rust-lld \
-              -Z linker-flavor=ld.lld \
-              -C link-arg=-Tlink.x
-    fi
 }
 
 main
