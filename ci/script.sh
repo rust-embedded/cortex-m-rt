cf6d460e8146f68e60764ae37b95a38a01366055
Emil Fresk <emil.fresk@gmail.com>
2018-08-28 21:42:28 +0200

    Moved execution of the markdown checks

--- ci/script.sh
+++ ci/script.sh
@@ -1,85 +1,84 @@
 set -euxo pipefail
 
 main() {
-    if [ $TARGET = docs ]; then
+    cargo check --target $TARGET
+
+    cargo check --target $TARGET --features device
+
+    local examples=(
+        alignment
+        minimal
+        main
+        override-exception
+        pre_init
+        state
+    )
+    local fail_examples=(
+        data_overflow
+    )
+    if [ $TRAVIS_RUST_VERSION = nightly ]; then
+        # linking with GNU LD
+        for ex in "${examples[@]}"; do
+            cargo rustc --target $TARGET --example $ex -- \
+                  -C linker=arm-none-eabi-ld \
+                  -C link-arg=-Tlink.x
+
+            cargo rustc --target $TARGET --example $ex --release -- \
+                  -C linker=arm-none-eabi-ld \
+                  -C link-arg=-Tlink.x
+        done
+        for ex in "${fail_examples[@]}"; do
+            ! cargo rustc --target $TARGET --example $ex -- \
+                  -C linker=arm-none-eabi-ld \
+                  -C link-arg=-Tlink.x
+
+            ! cargo rustc --target $TARGET --example $ex --release -- \
+                  -C linker=arm-none-eabi-ld \
+                  -C link-arg=-Tlink.x
+        done
+
+        cargo rustc --target $TARGET --example device --features device -- \
+              -C linker=arm-none-eabi-ld \
+              -C link-arg=-Tlink.x
+
+        cargo rustc --target $TARGET --example device --features device --release -- \
+              -C linker=arm-none-eabi-ld \
+              -C link-arg=-Tlink.x
+
+        # linking with rustc's LLD
+        for ex in "${examples[@]}"; do
+            cargo rustc --target $TARGET --example $ex -- \
+                  -C linker=rust-lld \
+                  -C link-arg=-Tlink.x
+
+            cargo rustc --target $TARGET --example $ex --release -- \
+                  -C linker=rust-lld \
+                  -C link-arg=-Tlink.x
+        done
+        for ex in "${fail_examples[@]}"; do
+            ! cargo rustc --target $TARGET --example $ex -- \
+                  -C linker=rust-lld \
+                  -C link-arg=-Tlink.x
+
+            ! cargo rustc --target $TARGET --example $ex --release -- \
+                  -C linker=rust-lld \
+                  -C link-arg=-Tlink.x
+        done
+
+        cargo rustc --target $TARGET --example device --features device -- \
+              -C linker=rust-lld \
+              -C link-arg=-Tlink.x
+
+        cargo rustc --target $TARGET --example device --features device --release -- \
+              -C linker=rust-lld \
+              -C link-arg=-Tlink.x
+    fi
+
+    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+        ./check-blobs.sh
+
+        # Check docs
         find . -name '*.md' -print0 | xargs -0 mdcheckr
-    else
-        cargo check --target $TARGET
-
-        cargo check --target $TARGET --features device
-
-        local examples=(
-            alignment
-            minimal
-            main
-            override-exception
-            pre_init
-            state
-        )
-        local fail_examples=(
-            data_overflow
-        )
-        if [ $TRAVIS_RUST_VERSION = nightly ]; then
-            # linking with GNU LD
-            for ex in "${examples[@]}"; do
-                cargo rustc --target $TARGET --example $ex -- \
-                      -C linker=arm-none-eabi-ld \
-                      -C link-arg=-Tlink.x
-
-                cargo rustc --target $TARGET --example $ex --release -- \
-                      -C linker=arm-none-eabi-ld \
-                      -C link-arg=-Tlink.x
-            done
-            for ex in "${fail_examples[@]}"; do
-                ! cargo rustc --target $TARGET --example $ex -- \
-                      -C linker=arm-none-eabi-ld \
-                      -C link-arg=-Tlink.x
-
-                ! cargo rustc --target $TARGET --example $ex --release -- \
-                      -C linker=arm-none-eabi-ld \
-                      -C link-arg=-Tlink.x
-            done
-
-            cargo rustc --target $TARGET --example device --features device -- \
-                  -C linker=arm-none-eabi-ld \
-                  -C link-arg=-Tlink.x
-
-            cargo rustc --target $TARGET --example device --features device --release -- \
-                  -C linker=arm-none-eabi-ld \
-                  -C link-arg=-Tlink.x
-
-            # linking with rustc's LLD
-            for ex in "${examples[@]}"; do
-                cargo rustc --target $TARGET --example $ex -- \
-                      -C linker=rust-lld \
-                      -C link-arg=-Tlink.x
-
-                cargo rustc --target $TARGET --example $ex --release -- \
-                      -C linker=rust-lld \
-                      -C link-arg=-Tlink.x
-            done
-            for ex in "${fail_examples[@]}"; do
-                ! cargo rustc --target $TARGET --example $ex -- \
-                      -C linker=rust-lld \
-                      -C link-arg=-Tlink.x
-
-                cargo rustc --target $TARGET --example device --features device --release -- \
-                      -C linker=rust-lld \
-                      -C link-arg=-Tlink.x
-            done
-
-            cargo rustc --target $TARGET --example device --features device -- \
-                  -C linker=rust-lld \
-                  -C link-arg=-Tlink.x
-
-            cargo rustc --target $TARGET --example device --features device --release -- \
-                  -C linker=rust-lld \
-                  -C link-arg=-Tlink.x
-        fi
-
-        if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-            ./check-blobs.sh
-        fi
     fi
 }
 
