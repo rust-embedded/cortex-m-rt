561d4f17e7b553baf1eea25321540a91aa1d54b5
Jorge Aparicio <jorge@japaric.io>
2018-09-06 16:49:44 +0200

    0.6.0

---
+++ 0.6.0/aliases.js
@@ -0,0 +1,11 @@
+var ALIASES = {};
+ALIASES['cortex_m_rt'] = {};
+ALIASES['cortex_m_rt_macros'] = {};
+ALIASES['libc'] = {};
+ALIASES['proc_macro2'] = {};
+ALIASES['quote'] = {};
+ALIASES['r0'] = {};
+ALIASES['rand'] = {};
+ALIASES['rand_core'] = {};
+ALIASES['syn'] = {};
+ALIASES['unicode_xid'] = {};
